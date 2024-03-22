945179c84544d0ce5189a8727901a81d9cdad43a
James Douglass <jdouglass@stanford.edu>
2024-03-21 18:06:54 -0700

    Adding a script to copy files and tiles to oak.

---
+++ data-cache/procured-sources/usgs-gmted2010/copy-to-oak.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+TARGET_DIR="$OAK/global-dataset-cache/usgs-gmted2010"
+mkdir -p "$TARGET_DIR" || echo "$TARGET_DIR already exists"
+
+cp -rv $SCRATCH/gmted2010/*.{tif,ovr,xyztiles} "$TARGET_DIR"
