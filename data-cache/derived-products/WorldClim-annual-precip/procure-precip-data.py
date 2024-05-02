511595bd10bb809a51213d70d59b0be08275a146
empavia <59708667+empavia@users.noreply.github.com>
2024-05-02 16:17:00 -0700

    update to main arguments

--- data-cache/derived-products/WorldClim-annual-precip/procure-precip-data.py
+++ data-cache/derived-products/WorldClim-annual-precip/procure-precip-data.py
@@ -3,13 +3,18 @@ import requests
 import io
 import zipfile
 from osgeo import gdal
-import logging
+import sys
+import glob
+from pathlib import Path
 import numpy
 import pygeoprocessing
+import logging
 
 logging.basicConfig(level=logging.INFO)
-path = 'Z:/global-dataset-cache/world-clim'
+file_path = sys.argv[1]
 url = r'https://biogeo.ucdavis.edu/data/worldclim/v2.1/base/wc2.1_30s_prec.zip'
+NODATA_FLOAT32 = numpy.finfo(numpy.float32).min
+
 directories = {'monthly':'monthly_path','annual':'annual_path'}
 for d,p in directories.items():
     path = os.path.join(file_path, d)            
@@ -18,7 +23,7 @@ for d,p in directories.items():
     globals()[str(p)] = path
         
 def read_url():
-    r = requests.get(url)
+    r = requests.get(url,stream=True)
     z = zipfile.ZipFile(io.BytesIO(r.content))
     z.extractall(monthly_path)
 
@@ -47,11 +52,12 @@ def sum_annual_precip():
      #   raster_paths, _sum, target_path, gdal.GDT_Float32, float(NODATA_FLOAT32))
 
 
-def main():
+def main(file_path):
+
     #read_url()
     sum_annual_precip()
 
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv[1])
     
