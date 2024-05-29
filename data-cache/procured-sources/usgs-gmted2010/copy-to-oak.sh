95cd62e3bcd209ed467531b16e54e7f7c60de390
empavia <epavia@stanford.com>
2024-05-28 19:58:06 -0700

    add copy to oak on sbatch

--- data-cache/procured-sources/usgs-gmted2010/copy-to-oak.sh
+++ data-cache/procured-sources/usgs-gmted2010/copy-to-oak.sh
@@ -3,4 +3,5 @@
 TARGET_DIR="$OAK/global-dataset-cache/usgs-gmted2010"
 mkdir -p "$TARGET_DIR" || echo "$TARGET_DIR already exists"
 
-cp -rv $SCRATCH/gmted2010/*.{tif,ovr,xyztiles} "$TARGET_DIR"
+#cp -rv $SCRATCH/gmted2010/*.{tif,ovr,xyztiles} "$TARGET_DIR"
+cp -rv $SCRATCH/gmted2010/*.xyztiles "$TARGET_DIR"
