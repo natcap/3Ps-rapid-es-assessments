ddae00ed16ca796462be2c590b9100c520db5d62
James Douglass <jdouglass@stanford.edu>
2024-06-11 16:17:20 -0700

    Reworking the bash script.

--- scripts/armenia-esm-netcdf-conversion/copy-esm-files-to-local-gdrive.sh
+++ scripts/armenia-esm-netcdf-conversion/copy-esm-files-to-local-gdrive.sh
@@ -1,16 +1,25 @@
 #!/usr/bin/env sh
 
-set -e  # This should cause things to fail if any command fails, like if a file is missing from GDrive.
-
 # Where GDrive is mounted on James' computer
 GDRIVE_DEFAULT_LOC="/Users/jdouglass/Library/CloudStorage/GoogleDrive-jadoug06@stanford.edu/Shared drives/GEF_GreenFin/Pilot_Countries/Armenia/GIS_Armenia/NatCap_Armenia_DataSharing/climate_projections/GFDL-ESM4 monthly processed data"
 
 GDRIVE_LOC=${1:-$GDRIVE_DEFAULT_LOC}
 
-find "$GDRIVE_LOC" -name "GFDL-ESM4_hist_plus*.tif" -o "*.csv"> files-on-gdrive.txt
+GTIFF_PATTERN="GFDL-ESM4_hist_plus*.tif"
+CSV_PATTERN="*.csv"
 
-for file in $(find . -name "$FILE_PATTERN")
+find "$GDRIVE_LOC" -name "$GTIFF_PATTERN" -o -name "$CSV_PATTERN" > files-on-gdrive.txt
+find $(pwd) -name "$GTIFF_PATTERN" -o -name "$CSV_PATTERN" > files-on-local.txt
+
+cat files-on-local.txt | while read file
 do
-    # If the file already exists on GDrive, copy it there. Otherwise, copy it to the GDrive root.
-    cp -v "$(pwd)/$file" "$(grep $file files-on-gdrive.txt || echo $GRIVE_LOC/$file)"
+    file_basename="$(basename "$file")"
+    target_path=$(grep "$file_basename" files-on-gdrive.txt)
+    if [ -z "$target_path" ]; then
+        new_file_on_gdrive="$GDRIVE_LOC/$file_basename"
+    else
+        new_file_on_gdrive="$target_path"
+    fi
+
+    cp -v "$file" "$new_file_on_gdrive"
 done
