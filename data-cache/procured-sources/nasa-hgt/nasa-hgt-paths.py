7d3de641dae64b8a122354aede14252ead1ed989
empavia <epavia@stanford.com>
2024-06-12 12:01:16 -0700

    add logging

--- data-cache/procured-sources/nasa-hgt/nasa-hgt-paths.py
+++ data-cache/procured-sources/nasa-hgt/nasa-hgt-paths.py
@@ -2,6 +2,7 @@ import os
 import sys
 import zipfile
 import glob
+import logging
 
 logging.basicConfig(level=logging.INFO)
 LOGGER = logging.getLogger(os.path.basename(__file__))
@@ -23,11 +24,12 @@ def extract_paths(tiles_dir):
     files = []
     for zip in glob.glob(os.path.join(tiles_dir, "*.zip"), recursive=True):
         with zipfile.ZipFile(zip, 'r') as zipObj:
+            print(zipObj)
             zipObj.testzip()
+            #<zipfile.ZipFile filename='/scratch/users/epavia/nasa-hgt-v1-1s/tiles/NASADEM_HGT_n60e080.zip' mode='r'>
             for name in zipObj.infolist():
-                LOGGER.info(f"Printing {name}")
-                print(name)
                 if name.filename.endswith(".hgt"):
+                    print(name.filename)
                     file = str(name.filename)
                     final_path = f'/vsizip/{zip}/{file}'
                     files.append(final_path)
