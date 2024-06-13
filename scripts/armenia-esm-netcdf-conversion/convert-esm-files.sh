cdc94cee79710293779111bab2792bcd193712a1
James Douglass <jdouglass@stanford.edu>
2024-06-13 13:06:12 -0700

    Adding new aggregation step to the orchestration shell script.

--- scripts/armenia-esm-netcdf-conversion/convert-esm-files.sh
+++ scripts/armenia-esm-netcdf-conversion/convert-esm-files.sh
@@ -13,19 +13,24 @@ fi
 
 SUFFIXES=("ssp126" "ssp585")
 YEARRANGES=("1961:1990" "2040:2040" "2041:2070" "2071:2100")
+WORKSPACE=${2:-$(pwd)}
 
 for suffix in ${SUFFIXES[@]}
 do
     for yearrange in ${YEARRANGES[@]}
     do
         python convert-daily-esm-netcdf-to-monthly-gtiff.py \
-            "$DIR/GFDL-ESM4_hist_plus_${suffix}_pet.nc" "$yearrange" "pet"
+            "$DIR/GFDL-ESM4_hist_plus_${suffix}_pet.nc" "$yearrange" "pet" "$WORKSPACE"
 
         python convert-daily-esm-netcdf-to-monthly-gtiff.py \
-            "$DIR/GFDL-ESM4_hist_plus_${suffix}_pr.nc" "$yearrange" "precip"
+            "$DIR/GFDL-ESM4_hist_plus_${suffix}_pr.nc" "$yearrange" "precip" "$WORKSPACE"
+        python aggregate-rain-events-by-climate-zone.py \
+            "$WORKSPACE/GFDL-ESM4_hist_plus_${suffix}_rain-events-by-cz.csv" \
+            "$DIR/KG_climatezones.tif" \
+            "$WORKSPACE/GFDL-ESM4_hist_plus_${suffix}_pr-$(echo $yearrange | tr : -)-*-rain-events.tif" \
 
         python convert-daily-esm-netcdf-to-monthly-gtiff.py \
-            "$DIR/GFDL-ESM4_hist_plus_${suffix}_tas.nc" "$yearrange" "tas"
+            "$DIR/GFDL-ESM4_hist_plus_${suffix}_tas.nc" "$yearrange" "tas" "$WORKSPACE"
     done
 done
 
