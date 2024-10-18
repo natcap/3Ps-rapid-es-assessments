70ab4f1d8a061a6009e5fddeab22968cb1a7e943
empavia <epavia@stanford.com>
2024-10-18 13:25:22 -0700

    update path and notes

--- scripts/tiff-to-cog-conversions/tif-to-cog.sbatch.sh
+++ scripts/tiff-to-cog-conversions/tif-to-cog.sbatch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-#SBATCH --time=5:00:00
+#SBATCH --time=8:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=2
 #SBATCH --mem-per-cpu=8G
@@ -13,7 +13,7 @@ module load python/3.9.0
 module load py-gdal-utils
 
 # update this path to reflect folders to search through to locate .tif files
-WORKDIR=$OAK/natcap-data-catalog-cache/footprint-impact-tool-data/
+WORKDIR=$OAK/global-dataset-cache/Public/
 N_WORKERS=8
 VALIDATION=/share/software/user/open/py-gdal-utils/3.4.1_py39/lib/python3.9/site-packages/osgeo_utils/samples/validate_cloud_optimized_geotiff.py
 
@@ -26,7 +26,9 @@ for t in $(cat tif_paths.txt);
         j=$j"_cog.tif"; \
         echo $t; \
         GDAL_CACHEMAX=2048 gdalinfo -stats $t; \
+        echo "Translating $t"
 		GDAL_CACHEMAX=2048 gdal_translate ${t} ${j} -of cog -strict -co "BIGTIFF=YES" -co "NUM_THREADS=$N_WORKERS"; \
+        echo "Checking validity of $j"
         GDAL_CACHEMAX=2048 python3 $VALIDATION $j >> $WORKDIR/validation_check.txt; \
 		python3 scale-check.py $j $t >> $WORKDIR/scale-check.txt; \
 	done
