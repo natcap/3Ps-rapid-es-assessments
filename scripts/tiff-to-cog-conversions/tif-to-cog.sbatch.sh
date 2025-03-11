bf9fd5fcfeb08a3f4cc391a154ff1f6864f9c8bf
empavia <epavia@stanford.com>
2025-03-11 13:31:38 -0700

    update to paths and correct overview creation

--- scripts/tiff-to-cog-conversions/tif-to-cog.sbatch.sh
+++ scripts/tiff-to-cog-conversions/tif-to-cog.sbatch.sh
@@ -13,28 +13,32 @@ module load python/3.9.0
 module load py-gdal-utils
 
 # update this path to reflect folders to search through to locate .tif files
-WORKDIR=$OAK/global-dataset-cache/Public/
+WORKDIR=$OAK/global-dataset-cache/Global_External/Public/CIESIN_NASA_GGRDI_v1/raw_downloads
 N_WORKERS=8
 VALIDATION=/share/software/user/open/py-gdal-utils/3.4.1_py39/lib/python3.9/site-packages/osgeo_utils/samples/validate_cloud_optimized_geotiff.py
 
 # create list of geotiffs
-#python3 list-geotiffs.py $WORKDIR > tif_paths.txt
+#python3 list-geotiffs.py $WORKDIR >> tif_paths.txt
 
 for t in $(cat tif_paths.txt); 
     do
-		j=`echo $t | cut -d . -f 1`; \
+		i=`echo ${t%.*}`; \
+        i=$i"_translate.tif"; \
+        j=`echo ${t%.*}`; \
         j=$j"_cog.tif"; \
         echo $t; \
+        echo $i; \
+        echo $j; \
         if [ -f $j ]; then 
             echo "$j Already Exists"
         else
-            GDAL_CACHEMAX=2048 gdalinfo -stats $t; \
-            GDAL_CACHEMAX=2048 gdaladdo $t; \
+            GDAL_CACHEMAX=2048 gdal_translate -of GTiff -co "TILED=YES" -co "BIGTIFF=YES" $t $i; \
+            GDAL_CACHEMAX=2048 gdaladdo $i; \
             echo "Translating $t"
-		    GDAL_CACHEMAX=2048 gdal_translate ${t} ${j} -of GTiff -strict -co COPY_SRC_OVERVIEWS=YES -co "BIGTIFF=YES" -co "TILED=YES" -co "COMPRESS=LZW" -co "NUM_THREADS=$N_WORKERS"; \
-            cogger ${j}
+		    GDAL_CACHEMAX=2048 gdal_translate ${i} ${j} -of GTiff -strict -co COPY_SRC_OVERVIEWS=YES -co "BIGTIFF=YES" -co "TILED=YES" -co "COMPRESS=LZW" -co "NUM_THREADS=$N_WORKERS"; \
+            cogger ${j}; \
+            echo "Checking validity of $j"
+            GDAL_CACHEMAX=2048 python3 $VALIDATION $j >> $WORKDIR/validation_check.txt; \
+		    python3 scale-check.py $j $t >> $WORKDIR/scale-check.txt
         fi
-        echo "Checking validity of $j"
-        GDAL_CACHEMAX=2048 python3 $VALIDATION $j >> $WORKDIR/validation_check.txt; \
-		python3 scale-check.py $j $t >> $WORKDIR/scale-check.txt; \
 	done
