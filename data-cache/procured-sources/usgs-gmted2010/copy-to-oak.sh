2ee97eab64a236fe2d9092fa7fc8e9205ed9e3bc
empavia <59708667+empavia@users.noreply.github.com>
2025-05-23 13:35:50 -0700

    remove scripts for Data Hub procurement that were moved to data-management repo

--- data-cache/procured-sources/usgs-gmted2010/copy-to-oak.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-TARGET_DIR="$OAK/global-dataset-cache/usgs-gmted2010"
-mkdir -p "$TARGET_DIR" || echo "$TARGET_DIR already exists"
-
-#cp -rv $SCRATCH/gmted2010/*.{tif,ovr,xyztiles} "$TARGET_DIR"
-cp -rv $SCRATCH/gmted2010/*.xyztiles "$TARGET_DIR"
